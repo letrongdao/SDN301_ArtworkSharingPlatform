7058bcda047e85f4f8b10ec583b817a3e451345c
DANG NGUYEN <noplstks@gmail.com>
2024-01-25 23:33:25 +0700

    API User

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log('Server started on port ' + PORT + '...');
